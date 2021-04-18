afd042f24cab4c3ec726397b8dd6bfbd5f0f988a
VGangarapu <VGangarapu@FAI-BLR02L1065.intl.corp.firstam.com>
2021-04-18 20:28:25 +0530

    FirstCommit

---
+++ HelloWorld_Sesha1/src/test/java/TestHelloWorld_Sesha/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package TestHelloWorld_Sesha;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+
+	public void TestHelloWorld(){
+		
+		System.out.println(" !... Hello World..Sesha Gangarapu ..! ") ;			
+		
+	}
+	
+}
