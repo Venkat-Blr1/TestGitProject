ed1fd79679466053da6b2eb175e1287934c6f6fd
VGangarapu <VGangarapu@FAI-BLR02L1065.intl.corp.firstam.com>
2021-04-20 14:48:21 +0530

    second hello world

---
+++ TestHellowworld_sesha2/src/test/java/testhellowworld_sesha2/Testhellowworld2.java
@@ -0,0 +1,14 @@
+package testhellowworld_sesha2;
+
+import org.testng.annotations.Test;
+
+public class Testhellowworld2 {
+	
+	@Test
+	public void hellowworld2() {
+		
+		System.out.println(" !... Hello second World.. Sesha Gangarapu ..! ") ;	
+		
+	}
+
+}
