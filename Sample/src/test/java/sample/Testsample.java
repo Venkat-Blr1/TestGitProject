e3d72ed186c95733fb49697c8886c151e92128f8
VGangarapu <VGangarapu@FAI-BLR02L1065.intl.corp.firstam.com>
2021-04-23 12:05:56 +0530

    Sample project code

---
+++ Sample/src/test/java/sample/Testsample.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Testsample {
+	
+	@Test
+	
+    public void TestHelloWorld()
+    {
+				System.out.println(" !... Hello First. World.First. Sesha Gangarapu ..! ") ;
+
+}
+    
+}
